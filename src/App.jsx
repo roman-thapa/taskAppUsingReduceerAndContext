9cd47b104b9a99b795b0fba877c3177143acc978
roman <51534566+roman-thapa@users.noreply.github.com>
2024-03-07 10:28:55 +0530

    Make the task app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-function App() {
+import React from 'react';
+import AddTask from './AddTask.jsx';
+import TaskList from './TaskList.jsx';
+import { TasksProvider } from './Context/TasksContent.jsx';
 
+export default function TaskApp() {
   return (
-    <>
-    </>
-  )
+    <TasksProvider>
+      <h1>Task</h1>
+      <AddTask />
+      <TaskList />
+    </TasksProvider>
+  );
 }
-
-export default App
